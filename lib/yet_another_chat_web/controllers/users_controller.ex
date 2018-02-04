ee46729a8a61edce32ba53d96a05d3bfb38a7df1
roman <roman@telichk.in>
2018-02-04 21:10:29 +0300

    Can't create user without all required fields

--- lib/yet_another_chat_web/controllers/users_controller.ex
+++ lib/yet_another_chat_web/controllers/users_controller.ex
@@ -1,9 +1,14 @@
 defmodule YetAnotherChatWeb.UsersController do
   use YetAnotherChatWeb, :controller
 
-  def create(conn, _params) do
+  def create(conn, %{"name" => _, "email" => _, "password" => _}) do
     conn
     |> redirect(to: page_path(YetAnotherChatWeb.Endpoint, :index))
+  end
+  def create(conn, %{}) do
+    conn
+    |> put_flash(:error, "Name can't be blank \nEmail can't be blank \nPassword can't be blank")
+    |> render(:create)
   end
 
   def show(conn, %{"name" => name}) do
--- test/yet_another_chat_web/controllers/user_controller_test.exs
+++ test/yet_another_chat_web/controllers/user_controller_test.exs
@@ -1,11 +1,20 @@
 defmodule YetAnotherChatWeb.UserControllerTest do
   use YetAnotherChatWeb.ConnCase
 
-  test "create user with email and password", %{conn: conn} do
-    conn = post(conn, "/users", %{"name" => "Roman", "password" => "StrongPWD!"})
+  test "create a user with name, email and password", %{conn: conn} do
+    conn = post(conn, "/users", %{"name" => "Roman", "email" => "some@mail.com", "password" => "StrongPWD!"})
     assert redirected_to(conn) =~ "/"
 
     conn = get(conn, "/users/Roman")
     assert html_response(conn, 200) =~ "Hello, Roman"
   end
+
+  test "can't create a user without email or name or password", %{conn: conn} do
+    conn = post(conn, "/users", %{})
+    assert html_response(conn, 200) =~ "Name can&#39;t be blank"
+    assert html_response(conn, 200) =~ "Email can&#39;t be blank"
+    assert html_response(conn, 200) =~ "Password can&#39;t be blank"    
+  end
+
+  test "can't create a user when name already exists"
 end
